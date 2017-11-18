689912c8343540d382b4de07bdee5d43ed229b13
pa7lux <pa7lux@gmail.com>
2017-11-18 19:13:36 +0300

    Rotating the earth — in progress

--- day3/in_class/02-project-upgrade/js/script.js
+++ day3/in_class/02-project-upgrade/js/script.js
@@ -4,13 +4,36 @@ window.addEventListener('load',function(){
 	let myCoolEarth = document.querySelector('.earth')
 	let leftArrow = document.querySelector('.leftArrow')
 
+	let animationPlayState = getComputedStyle(myCoolEarth).animationPlayState
+
 	rightArrow.addEventListener('click', function(){
-		myCoolEarth.style.animationPlayState = 'running'
-		rightArrow.style.backgroundImage = 'url("img/pause.png")'
+		if(animationPlayState != 'running') {
+			animationPlayState = 'running'
+			myCoolEarth.style.animationPlayState = animationPlayState
+			myCoolEarth.classList.remove('earth-animation-reverse')
+			myCoolEarth.classList.add('earth-animation')
+			this.style.backgroundImage = 'url("img/pause.png")'
+		} else {
+			animationPlayState = 'paused'
+			myCoolEarth.style.animationPlayState = animationPlayState
+			this.style.backgroundImage = 'url("img/rotate_right_arrow.png")'
+			// myCoolEarth.classList.remove('earth-animation')
+		}
 	})
 
 	leftArrow.addEventListener('click', function(){
-		
+		if(animationPlayState != 'running') {
+			animationPlayState = 'running'
+			myCoolEarth.style.animationPlayState = animationPlayState
+			myCoolEarth.classList.remove('earth-animation')
+			myCoolEarth.classList.add('earth-animation-reverse')
+			this.style.backgroundImage = 'url("img/pause.png")'
+		} else {
+			animationPlayState = 'paused'
+			myCoolEarth.style.animationPlayState = animationPlayState
+			this.style.backgroundImage = 'url("img/rotate_left_arrow.png")'
+			// myCoolEarth.classList.remove('earth-animation')
+		}
 	})
 
 	const homeBtn = document.querySelector('.home')
@@ -23,5 +46,3 @@ window.addEventListener('load',function(){
 		popUp.style.display = 'none'
 	})
 })
-
-
